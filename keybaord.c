24572da1210c776948beb6b6c309c84dd0d2148f
Gurmann Singh Jaggi <102686601+meilleur04@users.noreply.github.com>
2024-04-03 15:22:27 -0400

    Update keybaord.c

--- keybaord.c
+++ keybaord.c
@@ -1,18 +1,1 @@
-int main() {
-  volatile int *LEDR_ptr = 0xFF200000;
-  volatile int *KEYs = 0xff200050;
-  int value;
-  int edge_cap;
 
-  while (1) {
-    edge_cap = *(KEYs + 3);
-    if (edge_cap == 1) {
-      *LEDR_ptr = 0x3FF;
-      *(KEYs + 3) = 0x1;
-    }
-    if (edge_cap == 2) {
-      *LEDR_ptr = 0x0;
-      *(KEYs + 3) = 0x2;
-    }
-  }
-}
